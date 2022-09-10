c36cd9815aa1a019900d325ab9bff4d5859b74bb
Zachary Derrick <derrick.zachary@gmail.com>
2022-09-10 04:16:44 +0000

    a simple Python script that uses lists and prints from it.  Also it changes the value of an element

---
+++ pythoncollections.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Variables
+
+pirates = ["Stede", "Blackbeard", "Izzy", "Jim", "Buttons", "Frenchie", "Oluwande", "Lucius", "Wee John", "Ivan"]
+
+# Main
+
+print(pirates[3])
+print(pirates[6:11])
+pirates[6] = "onion"
+# print(pirates[6])
+# End 
